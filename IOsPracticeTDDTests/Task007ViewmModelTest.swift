98f7a8c4a3bb3cd7a17503635df668e18f626893
hovhannes <johnnysc091@gmail.com>
2024-12-12 00:17:00 +0900

    task007 ViewModelTest

---
+++ IOsPracticeTDDTests/Task007ViewmModelTest.swift
@@ -0,0 +1,76 @@
+//
+//  IOsPracticeTDDTests.swift
+//  IOsPracticeTDDTests
+//
+//  Created by Hovhannes on 27.11.2024.
+//
+
+import XCTest
+@testable import IOsPracticeTDD
+
+final class Task007ViewmModelTest: XCTestCase {
+
+    private var repository: FakeSimpleRepository!
+    private var viewModel: SimpleViewModel!
+    
+    override func setUpWithError() throws {
+        repository = FakeSimpleRepository()
+        viewModel = SimpleViewModel(repository: repository)
+    }
+    
+    func testViewModelSuccess() throws {
+        XCTAssertEqual(UiState(isLoadingNow: false, loadedData: "", isError: false), viewModel.uiState)
+        
+        viewModel.getData()
+        
+        XCTAssertEqual(UiState(isLoadingNow: true, loadedData: "", isError: false), viewModel.uiState)
+        
+        let expectation = expectation(description: "State should update")
+        Task.detached {
+            await MainActor.run {
+                XCTAssertEqual(UiState(isLoadingNow: false, loadedData: "mock data", isError: false), self.viewModel.uiState)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testViewModelError() throws {
+        repository.expectError()
+        XCTAssertEqual(UiState(isLoadingNow: false, loadedData: "", isError: false), viewModel.uiState)
+        
+        viewModel.getData()
+        
+        XCTAssertEqual(UiState(isLoadingNow: true, loadedData: "", isError: false), viewModel.uiState)
+        
+        let expectation = expectation(description: "State should update")
+        Task.detached {
+            await MainActor.run {
+                XCTAssertEqual(UiState(isLoadingNow: false, loadedData: "", isError: true), self.viewModel.uiState)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 1)
+    }
+}
+
+private class FakeSimpleRepository : SimpleRepository {
+    
+    private var shouldThrowError = false
+    
+    func expectError() {
+        shouldThrowError = true
+    }
+    func loadData() async throws -> String {
+        if shouldThrowError {
+            throw SimpleError()
+        } else {
+            return "mock data"
+        }
+    }
+}
+
+private class SimpleError : Error {
+}
